cdb2974d893adec8b1e861a5aa91b922934e9982
Amr Eldefrawy <eldefraw@amazon.com>
2019-02-09 12:42:01 +0000

    added register form

--- src/static/js/index.jsx
+++ src/static/js/index.jsx
@@ -1,27 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './App'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import PMForm from './pm_form'
+import {
+    BrowserRouter as Router, Link, Route
+} from 'react-router-dom'
 
-const Register = () => (
-    <div>
-      <h2>register</h2>
-      ...
-    </div>
-  )
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Collapse
+} from 'reactstrap'
 
 ReactDOM.render(
     <Router>
         <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand> HU Password Manager</NavbarBrand>
+
+                <Collapse navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink> <Link to={'/'}> Sign In</Link></NavLink>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLink>  <Link to={'/register'}>Register</Link></NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+
             <aside>
-                <Link to={'/'}> Sign In</Link>
-                <Link to={'/register'}>Register</Link>
+
+
             </aside>
 
 
             <main>
-                <Route exact path="/" component={App} />
-                <Route path="/register" component={Register} />
+                <Route exact path="/" render={() => (<PMForm header={<h2>Sign In</h2>} />)} />
+                <Route path="/register" render={() => (<PMForm header={<h2>Register</h2>} />)} />
             </main>
         </div>
     </Router>
---
+++ src/static/js/pm_form.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+import {
+    Col, Form,
+    FormGroup, Label, Input,
+    Button,
+} from 'reactstrap';
+
+import './App.css';
+
+export default class PMForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            validUserName: true,
+            validPassword: true,
+        }
+    }
+
+    validateUserName(e) {
+        const userName = e.target.value;
+
+        console.log(userName.length)
+        if (!userName || userName.length == 0 || userName.length < 5) {
+            this.setState({ validUserName: false });
+            return
+        }
+
+        // should only contains alphanumeric characters
+        const usernameRegex = /^[a-zA-Z0-9]+$/;
+        this.setState({ validUserName: usernameRegex.test(userName) });
+    }
+
+    validatePassword(e) {
+        const password = e.target.value;
+
+        if (!password || password.length == 0) {
+            this.setState({ validPassword: false });
+        }
+
+        // https://www.thepolyglotdeveloper.com/2015/05/use-regex-to-test-password-strength-in-javascript/
+        // The string must contain at least 1 numeric character, 1 uppercase character, one special character
+        // , alphabetical character and eight characters or longer
+        const passwordRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+        this.setState({ validPassword: passwordRegex.test(password) });
+    }
+
+    render() {
+        return (
+
+            <Form className="Form">
+                {this.props.header}
+                <Col>
+                    <FormGroup>
+                        <Label>Username</Label>
+                        <Input
+                            type="email"
+                            id="userNameInput"
+                            placeholder="my user name"
+                            onChange={e => {
+                                this.validateUserName(e)
+                            }}
+                            invalid={!this.state.validUserName}
+                        />
+                    </FormGroup>
+                </Col>
+                <Col>
+                    <FormGroup>
+                        <Label for="examplePassword">Password</Label>
+                        <Input
+                            type="password"
+                            id="passwordInput"
+                            placeholder="my password"
+                            onChange={e => {
+                                this.validatePassword(e)
+                            }}
+                            invalid={!this.state.validPassword}
+                        />
+                    </FormGroup>
+                </Col>
+                <Button>Submit</Button>
+            </Form>
+        );
+    }
+}
